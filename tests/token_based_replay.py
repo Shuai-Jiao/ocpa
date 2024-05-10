c726c088ee920de3fbe0f471ca3d95b5aedf57c7
Shuai-Jiao <jiao.shuai.1998.12.01@outlook.com>
2024-05-10 11:22:29 +0200

    update

--- tests/token_based_replay.py
+++ tests/token_based_replay.py
@@ -34,13 +34,14 @@ path_o2c = '/Users/jiao.shuai.1998.12.01outlook.com/Documents/ocpa/sample_logs/j
 path_o2c_2 = '/Users/jiao.shuai.1998.12.01outlook.com/Documents/ocpa/tests/sample_logs/filtered_BPI/o2c.jsonocel'
 path_p2p_2 = '/Users/jiao.shuai.1998.12.01outlook.com/Documents/OCEM/sample_logs/jsonocel/p2p-normal.jsonocel'
 tf_parameters = {'handle':True,'method':'S_component'}
+parameters1 = {'cache':{'bst':False,'activity':False},'token_flooding':{'handle':True,'method':'boundness'}}
 
-'''for path in [path_p2p_2,path_o2c_2][:1]:
+'''for path in [path_p2p,path_o2c]:
     ocel = ocel_import_factory.apply(path,parameters={"debug": False})
     cached_parameters = {'BST':False,'activity':True}
     ocpn = ocpn_discovery_factory.apply(ocel, parameters={"debug": False})
     time0= time.time() 
-    result = token_based_replay.apply(ocel,ocpn,method='flattened')
+    result = token_based_replay.apply(ocel,ocpn,method='object_centric',parameters=parameters1)
     time1= time.time()
     TBR_time = time1-time0 
     with open(text_path, 'a') as file:
@@ -50,7 +51,7 @@ tf_parameters = {'handle':True,'method':'S_component'}
 path = prefix+"BPI1to2executionprocess.jsonocel"
 ocel = ocel_import_factory.apply(path,parameters={"debug": False})
 ocpn = ocpn_discovery_factory.apply(ocel, parameters={"debug": False})
-for ele in log_list[2:7]:
+for ele in log_list[:5]:
     path = prefix+f"BPI{ele[0]}to{ele[1]}executionprocess.jsonocel"
     ocel = ocel_import_factory.apply(path,parameters={"debug": False})
     #ocpn = ocpn_discovery_factory.apply(ocel, parameters={"debug": False})
@@ -59,7 +60,7 @@ for ele in log_list[2:7]:
             print(f'output arcs: {[(a.target.name,a.target.label)for a in p.out_arcs]}')
     cached_parameters = {'BST':False,'activity':True}
     time0= time.time() 
-    result = token_based_replay.apply(ocel,ocpn,method='flattened')
+    result = token_based_replay.apply(ocel,ocpn,method='object_centric',parameters=parameters1)
     time1= time.time()
     TBR_time = time1-time0 
     out_path_info = current_dir+f"/output/information/info_{ele[0]}to{ele[1]}_log1.pkl"
